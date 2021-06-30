4f36f98c3c483103d4f23e98be0b9277ace698ed
nmarati <narayanan.marati@gmail.com>
2021-06-29 22:37:21 -0400

    Update pxt.json, main.blocks, main.ts, custom.ts

--- custom.ts
+++ custom.ts
@@ -4,59 +4,93 @@
  * Read more at https://makecode.microbit.org/blocks/custom
  */
 
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
-}
 
 /**
- * Custom blocks
+ * Kodely Turtle
  */
 //% weight=100 color=#0fbc11 icon=""
 namespace logoturtle {
-    /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
-     */
+ 
     //% block
     let _x = 2;
     let _y = 2;
+    let _prevx = 2;
+    let _prevy = 2;
+    let _prevpixel = 0;
     let _pen = 1;
     
     //% blockId=turtlehome
     //% block="goto start"
     export function home(): void {
         // Add code here
-        led.plotBrightness(_x, _y, 255)
+        _prevpixel = led.point(_x,_y)?1:0;
+        led.plotBrightness(_x, _y, 255);
 
     }
     //% blockId=turtleright
     //% block="turn right"
     export function right():void{
-        led.plotBrightness(_x,_y,100)
+
+        if (_pen) {
+            led.plotBrightness(_x,_y,100)
+        } else {
+            led.plotBrightness(_x, _y,_prevpixel*100 )
+        }
         _x = _x == 4?_x:_x+1;
+        _prevpixel = led.point(_x, _y)?1:0;
+        _prevx = _x;
+        led.plotBrightness(_x, _y, 255);
 
     }
     //% blockId=turtleleft
     //% block="turn left"
     export function left():void{
-        led.plotBrightness(_x,_y,100)
+        
+        if (_pen) {
+            led.plotBrightness(_x,_y,100)
+        } else {
+            led.plotBrightness(_x, _y,_prevpixel*100 )
+        }
         _x = _x == 0?_x:_x-1;  
+        _prevpixel = led.point(_x, _y)?1:0;
+        _prevx = _x;
+        led.plotBrightness(_x, _y, 255);
     }
     //% blockId=turtle up
     //% block="mvoe forward"
-    export function up():void{
-        led.plotBrightness(_x,_y,100)
+    export function forward():void{
+        if (_pen) {
+            led.plotBrightness(_x,_y,100)
+        } else {
+            led.plotBrightness(_x, _y,_prevpixel*100 )
+        }
         _y = _y == 0?_y:_y-1;  
+        _prevpixel = led.point(_x, _y)?1:0;
+        _prevy = _y;
+        led.plotBrightness(_x, _y, 255);
     }
     //% blockId=turtle down
     //% block="move down"
-    export function down():void{
-        led.plotBrightness(_x,_y,100)
+    export function backward():void{
+         if (_pen) {
+            led.plotBrightness(_x,_y,100)
+        } else {
+            led.plotBrightness(_x, _y,_prevpixel*100 )
+        }
         _y = _y == 4?_y:_y+1;  
+        _prevpixel = led.point(_x, _y)?1:0;
+        _prevy = _y;
+        led.plotBrightness(_x, _y, 255);
     }
+    //% blockId=turtle pen up
+    //% block="penUp"
+    export function penUp():void {
+        _pen = 0;
+    }
+    //% blockId=turtle pen down
+    //% block="penDown"
+    export function penDown():void {
+        _pen = 1;
+    }
+
 }
